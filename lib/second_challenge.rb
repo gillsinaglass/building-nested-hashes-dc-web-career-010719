fff9e9bb70c81d546e440102dd8e6d5b79e6d10d
Ryan Gill <rdgill95@gmail.com>
2019-01-03 21:56:10 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,14 +1,12 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :montague => {
       :patriarch => {},
       :matriarch => {},
       :hero => {},
       :hero_friends => []
       }, 
     :capulet => {
-      :montague => {
       :patriarch => {},
       :matriarch => {},
       :heroine => {},
